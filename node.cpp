651b74a5da61d02534c767d0e7ea7fcb9d2339e4
Niamh Driscoll <niamhmolson@outlook.com>
2025-10-07 14:15:13 -0700

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,1 +1,24 @@
+#include <iostream.h>
+#include <string>
+const int length = 32;
+typedef struct node_str
+{
+    std::stream value;
+    struct node *next;
+} node_str;
+typedef struct node_int
+{
+    int value;
+    struct node *next;
+} node_int;
+typedef struct node_bool
+{
+    bool value;
+    struct node *next;
+} node_bool;
+typedef struct node_char
+{
+    char value;
+    struct node *next;
+} node_char;
 
